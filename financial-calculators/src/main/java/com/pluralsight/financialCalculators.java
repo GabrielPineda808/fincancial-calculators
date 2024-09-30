ca24a3efd9b9a41ec8a3e91be3d1c728c28418d0
Gabe <gabepineda6@gmail.com>
2024-09-30 13:28:19 -0700

    Created a method that asks the user to select which calculator they want to use

--- financial-calculators/src/main/java/com/pluralsight/financialCalculators.java
+++ financial-calculators/src/main/java/com/pluralsight/financialCalculators.java
@@ -1,7 +1,18 @@
 package com.pluralsight;
+import java.util.Scanner;
+
 
 public class financialCalculators {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
+    }
+    public static void calcChoice(){
+        System.out.println("Hello please enter which calculator you would like to use below: \n"+
+        "Your calculator options include: \n" +
+        "( M ) Mortgage Calculator \n" +
+        "( C ) CD Maturity Value \n " +
+        "( A ) Ordinary Annuity Present Value");
+        String ans = scanner.nextLine();
     }
 }
