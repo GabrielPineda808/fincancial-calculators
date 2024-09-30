e9383853950bd1bd7a583a424fbce010529baab8
Gabe <gabepineda6@gmail.com>
2024-09-30 15:38:16 -0700

    Created a mortgage formula to calculate the monthly payment for a loan

--- financial-calculators/src/main/java/com/pluralsight/MortgageCalculator.java
+++ financial-calculators/src/main/java/com/pluralsight/MortgageCalculator.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class MortgageCalculator {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
+        mortgageCalculator();
     }
     public static void mortgageCalculator(){
         double p = answer("Please enter the principal amount of your mortgage loan: ");
         double i = answer("Please enter the interest amount of your mortgage loan: ");
-        double t = answer("Please enter the term length on your mortgage loan in months: ");
+        double t = answer("Please enter the term length on your mortgage loan in years: ");
+        monthly(p,i,t);
+
     }
 
     public static double answer(String message){
@@ -17,6 +19,18 @@ public class MortgageCalculator {
         double ans = scanner.nextDouble();
         return ans;
     }
+
+    public static void monthly(double p, double i, double t){
+        double n = 12*t;
+        double r = i/12;
+        double top = r*Math.pow((1+r), n);
+        double bottom = Math.pow((1+r),n) -1;
+        double fract = top / bottom;
+        double monthly = (p*fract)/100;
+
+        System.out.printf("Your monthly payment is $%.2f", monthly);
+    }
+
 
 
 }
