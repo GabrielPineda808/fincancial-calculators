727cfedae5de2ae5b8f7f6a822ca344fd4613db8
Gabe <gabepineda6@gmail.com>
2024-09-30 13:36:12 -0700

    Asked the user to select a calculator choice and saved their console response to a var. I checked tha var inside a switch statement and based on the case a certain method will be called

--- financial-calculators/src/main/java/com/pluralsight/financialCalculators.java
+++ financial-calculators/src/main/java/com/pluralsight/financialCalculators.java
@@ -5,14 +5,28 @@ import java.util.Scanner;
 public class financialCalculators {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
+        calcChoice();
     }
     public static void calcChoice(){
         System.out.println("Hello please enter which calculator you would like to use below: \n"+
         "Your calculator options include: \n" +
         "( M ) Mortgage Calculator \n" +
-        "( C ) CD Maturity Value \n " +
+        "( C ) CD Maturity Value \n" +
         "( A ) Ordinary Annuity Present Value");
         String ans = scanner.nextLine();
+
+        switch (ans.toUpperCase()){
+            case "M":
+                System.out.println("Mortgage");
+              break;
+            case "C":
+                System.out.println("CD");
+                break;
+            case "A":
+                System.out.println("Annuity");
+                break;
+            default:
+                System.out.println( "Please enter a proper calculator choice.");
+        }
     }
 }
