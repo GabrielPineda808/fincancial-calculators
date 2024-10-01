ea65958b710f847b2b671485dc159474b6b10fbe
Gabe <gabepineda6@gmail.com>
2024-10-01 10:00:51 -0700

    Created a method that calls on the mortgage answer method that was imported to save user input in order to place it into a formula

--- financial-calculators/src/main/java/com/pluralsight/AnnuityCalculator.java
+++ financial-calculators/src/main/java/com/pluralsight/AnnuityCalculator.java
@@ -1,7 +1,16 @@
 package com.pluralsight;
 
+import static com.pluralsight.MortgageCalculator.answer;
+
 public class AnnuityCalculator {
     public static void main(String[] args) {
-        
+
     }
+
+    public static void aCalc() {
+        double payout = answer("Please enter the monthly payout: ");
+        double i = answer("Please enter the expected interest rate: ");
+        double t = answer("Please enter the years until payout: ");
+    }
+
 }
